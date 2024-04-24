a2d100a6b143b72e955bcf7da1e145cae759d39d
Lilibeth <lilibethocando@gmail.com>
2024-04-24 00:07:30 -0500

    Removed Key

--- create_token.py
+++ create_token.py
@@ -2,12 +2,12 @@ import stripe
 import os
 
 # Set your Stripe API key
-stripe.api_key = "sk_test_51OCsCqCb8MXdJtc6GplIKp1r5ecaHUpKeTImWvUKE2SqMz12KlFvlTBBGTbYLjW8DVysEvTHStGhihDsPrprbrcz00I0KPZcF5"
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 try:
     # Create a payment with a test token
     stripe.PaymentIntent.create(
-        amount=2000,  # Amount in cents
+        amount=8000,  # Amount in cents
         currency="usd",
         payment_method_types=["card"],
         payment_method="pm_card_visa",  # Test card token
