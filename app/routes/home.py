ae482002a9fbd43765bb5e2c2a4fb4fe4e91ea9e
Lilibeth <lilibethocando@gmail.com>
2024-05-29 00:26:49 -0500

    Add static file

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,10 +1,10 @@
 import os
-from app import app
 from flask import render_template
+from app import app
 
 # Get the filename of the generated JavaScript bundle
-js_bundle_dir = os.path.abspath(os.path.dirname(__file__)) + '/static/assets'
-js_bundle_filename = os.listdir(js_bundle_dir)[0]  # Assuming there's only one file in the directory
+js_bundle_dir = os.path.join(app.static_folder, 'assets')
+js_bundle_filename = [f for f in os.listdir(js_bundle_dir) if f.endswith('.js')][0]  # Assuming there's only one .js file
 
 # Extract the hash from the filename (assuming the format is "index-{hash}.js")
 bundle_hash = js_bundle_filename.split('-')[1]
