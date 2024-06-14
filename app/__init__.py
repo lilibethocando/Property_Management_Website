c1478f7e77e45a98bfc61b0a5cbf26a52dde435a
Lilibeth <lilibethocando@gmail.com>
2024-06-14 11:24:25 -0500

    Some updates for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from config import Config
 import os
 
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
+app = Flask(__name__, static_folder='/property-management-frontend/build', static_url_path='')
+
+CORS(app, resources={r"/*": {"origins": ['http://localhost:5173', 'https://property-management-website.onrender.com']}}, supports_credentials=True)
 
 
 app.config.from_object(Config)
@@ -18,18 +19,23 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 from . import models
 from app.routes import home, apartment, request_by, tenant, user, complaint, payment, reminder, notification, auth, email
 
 @app.before_request
 def before_request():
     if request.method == 'OPTIONS':
-        response = jsonify({'message': 'Preflight request received'})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        return response, 200
+        response = app.make_default_options_response()
+        headers = request.headers.get('Access-Control-Request-Headers', '')
+        response.headers.add('Access-Control-Allow-Headers', headers)
+        response.headers.add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin', ''))
+        return response
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- app/routes/home.py
+++ app/routes/home.py
@@ -2,13 +2,13 @@ import os
 from flask import render_template
 from app import app
 
-# Get the filename of the generated JavaScript bundle
-js_bundle_dir = os.path.join(app.static_folder, 'assets')
-js_bundle_filename = [f for f in os.listdir(js_bundle_dir) if f.endswith('.js')][0]  # Assuming there's only one .js file
+# # Get the filename of the generated JavaScript bundle
+# js_bundle_dir = os.path.join(app.static_folder, 'assets')
+# js_bundle_filename = [f for f in os.listdir(js_bundle_dir) if f.endswith('.js')][0]  # Assuming there's only one .js file
 
-# Extract the hash from the filename (assuming the format is "index-{hash}.js")
-bundle_hash = js_bundle_filename.split('-')[1]
+# # Extract the hash from the filename (assuming the format is "index-{hash}.js")
+# bundle_hash = js_bundle_filename.split('-')[1]
 
 @app.route('/')
-def index():
-    return render_template('index.html', bundle_hash=bundle_hash)
+def home():
+    return render_template('index.html')
