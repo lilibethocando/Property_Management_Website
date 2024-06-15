027d197ba2403946f21116dcba0335070b8a3045
Lilibeth <lilibethocando@gmail.com>
2024-06-14 19:40:06 -0500

    Updated Axios configuration in AxiosProvider for environment-based base URL.

--- property-management-frontend/src/components/AxiosContext.jsx
+++ property-management-frontend/src/components/AxiosContext.jsx
@@ -3,10 +3,15 @@ import axios from 'axios';
 
 const AxiosContext = createContext();
 
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'https://property-management-website.onrender.com'
+    : 'http://localhost:5000';
+
 const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://property-management-website.onrender.com' : 'http://localhost:5000',
+    baseURL,
     withCredentials: true,
 });
+
 
 const AxiosProvider = ({ children }) => {
     return (
